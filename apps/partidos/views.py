85b132f9531e26fad8a55c159b7f89d3644dc74d
pablomv8 <pabmarval@alum.us.es>
2026-03-09 02:29:31 +0100

    Crear home

--- apps/partidos/views.py
+++ apps/partidos/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Jornada
+
+
+def home(request):
+    hoy = timezone.now().date()
+
+    # Buscar la próxima jornada
+    proxima_jornada = (
+        Jornada.objects
+        .filter(fecha_inicio__gt=hoy)
+        .order_by("fecha_inicio")
+        .first()
+    )
+
+    partidos = []
+
+    if proxima_jornada:
+        partidos = proxima_jornada.partidos.select_related("local", "visitante")
+
+    context = {
+        "jornada": proxima_jornada,
+        "partidos": partidos,
+    }
+
+    return render(request, "home.html", context)
--- core/urls.py
+++ core/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
+from apps.partidos.views import home
 
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('equipos/', include('apps.equipos.urls')),
     path('partidos/', include('apps.partidos.urls')),
